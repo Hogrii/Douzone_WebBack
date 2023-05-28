ca35a16640ce7702521d0c1d1c019ba0320e81a1
LJH <hosboy93@naver.com>
2023-05-28 15:24:05 +0900

    회원가입

---
+++ WebServlet_7_Member_Command/src/main/java/kr/or/kosa/controller/FrontRegisterController.java
@@ -0,0 +1,68 @@
+package kr.or.kosa.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.kosa.dao.RegisterDAO;
+import kr.or.kosa.dto.RegisterDTO;
+
+/**
+ * URL 방식(command 방식에도 상관 없다)
+ * http://192.168.0.172:8090/WebSite/list.do >> *.do
+ * http://192.168.0.172:8090/WebSite/write.do >> *.do
+ * 
+ * command 방식(parameter 업무 파악)
+ * @WebServlet("site.do")
+ * http://192.168.0.172:8090/WebSite/site.do?cmd=list
+ * http://192.168.0.172:8090/WebSite/site.do?cmd=write
+ */
+@WebServlet("*.do")
+public class FrontRegisterController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public FrontRegisterController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+	private void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 한글처리
+		request.setCharacterEncoding("UTF-8");
+		// 2. 데이터 받기
+		String cmd = request.getParameter("cmd");
+		// 3. 요청 판단하기
+		if(cmd.equals("register")) {
+			RequestDispatcher dis = request.getRequestDispatcher("/WEB-INF/views/register/register.jsp");
+			dis.forward(request, response);
+		}else if(cmd.equals("registerok")) {
+			RegisterDTO dto = new RegisterDTO();
+			dto.setId(Integer.parseInt(request.getParameter("id")));
+			dto.setPwd(request.getParameter("pwd"));
+			dto.setEmail(request.getParameter("email"));
+			
+			RegisterDAO dao = new RegisterDAO();
+			dao.writeOk(dto);
+			
+			// 4. 데이터 담기
+			request.setAttribute("user", dto);
+			// 5. 뷰 지정하기
+			RequestDispatcher dis = request.getRequestDispatcher("/WEB-INF/views/register/register_welcome.jsp");
+			// 6. 뷰 전송하기
+			dis.forward(request, response);
+		}
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+}
---
+++ WebServlet_7_Member_Command/src/main/webapp/WEB-INF/views/register/register.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>회원가입</title>
+</head>
+<body>
+	<form action="${pageContext.request.contextPath}/register.do?cmd=registerok" method="post">
+		ID : <input type="text" name="id" placeholder="아이디 입력" required="required"><br>
+		PWD : <input type="password" name="pwd" placeholder="비밀번호 입력" required="required"><br>
+		EMAIL : <input type="text" name="email" placeholder="이메일 입력" required="required"><br>
+		<input type="submit" value="회원가입">
+	</form>
+</body>
+</html>
